dd457dc275e6363e6eab6f73f64566429795054f
Chris Kim <christopher.cuong.kim@gmail.com>
2024-06-28 15:33:16 -0400

    Refactor Neovim for better plugins and formatting
    - Added autoformatting for various LSP servers: gopls, golines, goimports, gofumpt, black
    - Updated debug keymaps and configurations for DAP integration
    - Renamed and adjusted plugin configurations for clarity and consistency
    - Updated lazy-loading configuration for improved performance
    - Added custom autoformat.lua script for consistent code formatting across LSP clients

--- .config/nvim/lua/kickstart/plugins/debug.lua
+++ .config/nvim/lua/kickstart/plugins/debug.lua
@@ -46,12 +46,12 @@ return {
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
+    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<leader>dsi', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<leader>dsa', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<leader>dso', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>dB', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
 
@@ -78,7 +78,7 @@ return {
     }
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<leader>dui', dapui.toggle, { desc = 'Debug: See last session result.' })
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
@@ -92,5 +92,12 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+
+    vim.keymap.set('n', '<leader>dgt', function()
+      require('dap-go').debug_test()
+    end, { desc = 'Debug: go test' })
+    vim.keymap.set('n', '<leader>dgl', function()
+      require('dap-go').debug_last_test()
+    end, { desc = 'Debug:  last go test' })
   end,
 }
