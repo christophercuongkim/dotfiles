ae4630fa787f2aa74a0595fc58ca9a6d6d662d74
Chris Kim <christopher.cuong.kim@gmail.com>
2025-05-29 14:24:37 -0400

    initial flake.

---
+++ nix/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Package set for my system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        pkgs.git
+        pkgs.neovim
+        pkgs.fd
+        pkgs.bat
+        pkgs.go
+        pkgs.python3Full
+        pkgs.zsh
+        pkgs.oh-my-posh
+        pkgs.ripgrep
+        pkgs.fzf
+        pkgs.zoxide
+        pkgs.pyenv
+        pkgs.pipenv
+      ];
+    };
+  };
+}
